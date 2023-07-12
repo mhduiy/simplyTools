8c52d5299be5897051210366e546241d7f619e6a
zk <2840441575@qq.com>
2023-07-13 03:47:35 +0800

    feat: 新增消息弹窗

---
+++ src/customComponents/MNotificationBox.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mhduiy on 2023/7/13.
+//
+
+#include "MNotificationBox.h"
---
+++ src/customComponents/MNotificationBox.h
@@ -0,0 +1,16 @@
+//
+// Created by mhduiy on 2023/7/13.
+//
+
+#ifndef SIMPLYTOOLS_MNOTIFICATIONBOX_H
+#define SIMPLYTOOLS_MNOTIFICATIONBOX_H
+
+
+
+class MNotificationBox {
+
+};
+
+
+
+#endif //SIMPLYTOOLS_MNOTIFICATIONBOX_H
